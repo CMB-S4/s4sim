190325b40a3f24a9437e4ae3e226e2ba079e6cca
smsimon <smsimon@umich.edu>
2020-01-30 13:50:27 -0500

    Update pysm.py

--- s4sim/pipeline_tools/pysm.py
+++ s4sim/pipeline_tools/pysm.py
@@ -9,23 +9,42 @@ from toast.utils import Logger
 
 try:
     import pysm
-    import so_pysm_models
 except:
     pysm = None
-    so_pysm_models = None
+
+try:
+    import s4_pysm_models
+except Exception as e:
+    print('Failed to load s4_pysm_models: "{}"'.format(e))
+    s4_pysm_models = None
 
 
 def add_pysm_args(parser):
     parser.add_argument(
-        "--input-pysm-model",
+        "--pysm-model",
         required=False,
         help="Comma separated models for on-the-fly PySM "
         'simulation, e.g. s3,d6,f1,a2" '
-        "this pipeline also supports the SO specific models from "
+        "this pipeline also supports the S4 specific models from "
         "so_pysm_models, see https://github.com/simonsobs/so_pysm_models "
         "currently the most complete PySM model for simulations is:"
         '"SO_d0,SO_s0,SO_a0,SO_f0,SO_x1_cib,SO_x1_tsz,SO_x1_ksz,SO_x1_cmb_lensed_solardipole"',
     )
+    parser.add_argument(
+        "--pysm-apply-beam",
+        required=False,
+        action="store_true",
+        help="Convolve sky with detector beam",
+        dest="pysm_apply_beam",
+    )
+    parser.add_argument(
+        "--no-pysm-apply-beam",
+        required=False,
+        action="store_false",
+        help="Do not convolve sky with detector beam.",
+        dest="pysm_apply_beam",
+    )
+    parser.set_defaults(pysm_apply_beam=True)
     return
 
 
@@ -48,15 +67,17 @@ def simulate_sky_signal(args, comm, data, focalplanes, subnpix, localsm, signaln
     )
     pysm_component_objects = []
     pysm_model = []
-    for model_tag in args.input_pysm_model.split(","):
+    for model_tag in args.pysm_model.split(","):
 
-        if not model_tag.startswith("SO"):
+        if not model_tag.startswith("S4"):
             pysm_model.append(model_tag)
         else:
-
+            if s4_pysm_models is None:
+                raise RuntimeError(
+                    "{} requires s4_pysm_models".format(model_tag))
             if model_tag == "SO_x1_cib":
                 pysm_component_objects.append(
-                    so_pysm_models.WebSkyCIB(
+                    s4_pysm_models.WebSkyCIB(
                         websky_version="0.3",
                         interpolation_kind="linear",
                         nside=args.nside,
@@ -65,7 +86,7 @@ def simulate_sky_signal(args, comm, data, focalplanes, subnpix, localsm, signaln
                 )
             elif model_tag == "SO_x1_ksz":
                 pysm_component_objects.append(
-                    so_pysm_models.WebSkySZ(
+                    s4_pysm_models.WebSkySZ(
                         version="0.3",
                         nside=args.nside,
                         map_dist=map_dist,
@@ -74,7 +95,7 @@ def simulate_sky_signal(args, comm, data, focalplanes, subnpix, localsm, signaln
                 )
             elif model_tag == "SO_x1_tsz":
                 pysm_component_objects.append(
-                    so_pysm_models.WebSkySZ(
+                    s4_pysm_models.WebSkySZ(
                         version="0.3",
                         nside=args.nside,
                         map_dist=map_dist,
@@ -85,7 +106,7 @@ def simulate_sky_signal(args, comm, data, focalplanes, subnpix, localsm, signaln
                 lensed = "unlensed" not in model_tag
                 include_solar_dipole = "solar" in model_tag
                 pysm_component_objects.append(
-                    so_pysm_models.WebSkyCMBMap(
+                    s4_pysm_models.WebSkyCMBMap(
                         websky_version="0.3",
                         lensed=lensed,
                         include_solar_dipole=include_solar_dipole,
@@ -95,14 +116,16 @@ def simulate_sky_signal(args, comm, data, focalplanes, subnpix, localsm, signaln
                     )
                 )
             else:
+                if not model_tag.endswith("s") and args.nside > 512:
+                    model_tag += "s"
                 pysm_component_objects.append(
-                    so_pysm_models.get_so_models(
+                    s4_pysm_models.get_so_models(
                         model_tag, args.nside, map_dist=map_dist
                     )
                 )
 
     if signalname is None:
-        signalname = "signal"
+        signalname = "pysmsignal"
     op_sim_pysm = OpSimPySM(
         comm=comm.comm_rank,
         out=signalname,
@@ -112,11 +135,11 @@ def simulate_sky_signal(args, comm, data, focalplanes, subnpix, localsm, signaln
         nside=args.nside,
         subnpix=subnpix,
         localsm=localsm,
-        apply_beam=args.apply_beam,
+        apply_beam=args.pysm_apply_beam,
         coord="G",  # setting G doesn't perform any rotation
         map_dist=map_dist,
     )
-    assert args.coord in "CQ", "Input SO models are always in Equatorial coordinates"
+    assert args.coord in "CQ", "Input S4 models are always in Equatorial coordinates"
     op_sim_pysm.exec(data)
     if comm.comm_world is not None:
         comm.comm_world.barrier()
