a12da8ae300167ed88b8b6c09a4abcfa81054c2c
smsimon <smsimon@umich.edu>
2020-01-30 13:15:33 -0500

    Update __init__.py

--- s4sim/scripts/__init__.py
+++ s4sim/scripts/__init__.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2020-2020 CMB-S4 Collaboration.
 # Full license can be found in the top level "LICENSE" file.
-"""Simons Observatory TOD processing entry points.
+"""S4 TOD processing entry points.
 
 This module contains files which have a main() function defined, and which are
 defined as entry points in setup.py.
