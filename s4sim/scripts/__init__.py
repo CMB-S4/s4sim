dfbd58485c6bccc5064e608401273e4018568afc
smsimon <smsimon@umich.edu>
2020-01-30 12:50:04 -0500

    Add files via upload

--- s4sim/scripts/__init__.py
+++ s4sim/scripts/__init__.py
@@ -1,5 +1,8 @@
-# Copyright (c) 2020-2020 CMB-S4 Collaboration.
+# Copyright (c) 2018 Simons Observatory.
 # Full license can be found in the top level "LICENSE" file.
+"""Simons Observatory TOD processing entry points.
 
-"""s4sim command line scripts
+This module contains files which have a main() function defined, and which are
+defined as entry points in setup.py.
+
 """
