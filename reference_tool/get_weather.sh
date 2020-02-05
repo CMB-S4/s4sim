74f51a22a4f103c6cac0eb0aa51c6e2b33bbab59
Reijo Keskitalo <reijo.keskitalo@gmail.com>
2020-02-05 12:06:51 -0800

    fix get_weather.sh

--- reference_tool/get_weather.sh
+++ reference_tool/get_weather.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-wget -nv -r -c -N -np -nH --progress=bar http://portal.nersc.gov/project/cmb/toast_data/example_data/weather_South_Pole.fits
+wget --progress=bar http://portal.nersc.gov/project/cmb/toast_data/example_data/weather_South_Pole.fits
 
-wget -nv -r -c -N -np -nH --progress=bar http://portal.nersc.gov/project/cmb/toast_data/example_data/weather_Atacama.fits
+wget --progress=bar http://portal.nersc.gov/project/cmb/toast_data/example_data/weather_Atacama.fits
