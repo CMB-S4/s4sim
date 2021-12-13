a804ee4702c14c80780b986677d965187bc0daf1
Reijo Keskitalo <reijo.keskitalo@gmail.com>
2021-12-13 13:47:16 -0800

    script tweaks

--- dc1/cmb_sim/remove_timeouts.sh
+++ dc1/cmb_sim/remove_timeouts.sh
@@ -2,15 +2,8 @@
 
 mkdir -p failed_logs
 
-fnames=(`grep -lR --include '*.log' CANCELLED logs`)
+fnames=(`grep -lR --include '*.log' -e CANCELLED -e Terminated -e myquota -e client_io_handler_start logs`)
 echo "Removing ${#fnames[@]} cancelled logs"
-for fname in ${fnames[*]}; do
-    echo "Removing $fname"
-    mv $fname failed_logs/
-done
-
-fnames=(`grep -lR --include '*.log' Terminated logs`)
-echo "Removing ${#fnames[@]} terminated logs"
 for fname in ${fnames[*]}; do
     echo "Removing $fname"
     mv $fname failed_logs/
