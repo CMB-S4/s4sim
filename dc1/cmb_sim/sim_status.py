b8288e7aa47bc9ba21fc0559ee9425d85c49ba1f
Reijo Keskitalo <reijo.keskitalo@gmail.com>
2022-01-12 16:37:12 -0800

    upgrade parameter files

--- dc1/cmb_sim/sim_status.py
+++ dc1/cmb_sim/sim_status.py
@@ -6,13 +6,14 @@ import sys
 
 import numpy as np
 
-tele = "chlat"
-fnames = glob(f"split_schedules/{tele}/split_schedule_*.txt")
-fnames_done = glob(f"split_schedules/{tele}/done/split_schedule_*.txt")
-nsplit = len(fnames) + len(fnames_done)
-print(f"Found {nsplit} split schedule files")
+for tele in "chlat", "splat", "spsat":
+    print(f"\n{tele}\n")
+    fnames = glob(f"split_schedules/{tele}/split_schedule_*.txt")
+    fnames_done = glob(f"split_schedules/{tele}/done/split_schedule_*.txt")
+    nsplit = len(fnames) + len(fnames_done)
+    print(f"Found {nsplit} split schedule files")
 
-for freq in 30, 40, 90, 150, 220, 280:
-    fnames = glob(f"logs/chlat/f{freq:03}/*.log")
-    nlog = len(fnames)
-    print(f"{freq:3} GHz is {nlog:5} / {nsplit:5} = {nlog / nsplit * 100:6.3f}% complete")
+    for freq in 30, 40, 90, 150, 220, 280:
+        fnames = glob(f"logs/{tele}/f{freq:03}/*.log")
+        nlog = len(fnames)
+        print(f"{freq:3} GHz is {nlog:5} / {nsplit:5} = {nlog / nsplit * 100:6.3f}% complete")
