79d0c7ac54bef6c95124fb29d896ca28fba5accf
Reijo Keskitalo <reijo.keskitalo@gmail.com>
2022-01-27 08:27:12 -0800

    Check the fize size before purging

--- dc1/noise_sim/purge.sh
+++ dc1/noise_sim/purge.sh
@@ -2,12 +2,19 @@
 
 # Remove local files that have been synced to CFS
 
-fnames=(`find /global/cfs/cdirs/cmbs4/dc/dc1/staging/noise_sim/outputs -name "*.h5"`)
+fnames=(`find outputs -name "*.h5"`)
 
-for fname_copy in ${fnames[*]}; do
-    fname_orig=${fname_copy/\/global\/cfs\/cdirs\/cmbs4\/dc\/dc1\/staging\/noise_sim\/}
-    if [[ -e $fname_orig ]]; then
-        echo "Deleting $fname_orig"
-        rm -f $fname_orig
+for fname_orig in ${fnames[*]}; do
+    fname_copy=/global/cfs/cdirs/cmbs4/dc/dc1/staging/noise_sim/$fname_orig
+    if [[ -e $fname_copy ]]; then
+        size_orig=`wc -c $fname_orig | awk '{print $1}'`
+        size_copy=`wc -c $fname_copy | awk '{print $1}'`
+        if [[ $size_orig == $size_copy ]]; then
+            echo "Purging $fname_orig == $fname_copy"
+            rm -f $fname_orig
+        else
+            echo "MISMATCH : $fname_orig != $fname_copy"
+            echo "MISMATCH : ($size_orig != $size_copy)"
+        fi
     fi
 done
