20dc729f8d0a1da4525f25de44b5dd2c53e18159
Reijo Keskitalo <reijo.keskitalo@gmail.com>
2022-11-16 15:11:46 -0800

    small tweaks

--- dc1/foreground_sim/sim_status.py
+++ dc1/foreground_sim/sim_status.py
@@ -12,6 +12,9 @@ TELE = "LAT0_CHLAT"
 
 # logs/LAT0_CHLAT/f090/LAT0_CHLAT_split_schedule_0000_f090.log
 
+hits_tot = 0
+nsplit_tot = 0
+
 for subset in "upto2mm", "over2mm":
     print(f"\nsubset = {subset}\n")
     fnames_in = sorted(glob(f"split_schedules_1_{subset}/{tele}/*.txt"))
@@ -33,3 +36,9 @@ for subset in "upto2mm", "over2mm":
             if ob in obs:
                 hits += 1
         print(f"{freq:3} GHz is {hits:5} / {nsplit:5} = {hits / nsplit * 100:6.3f}% complete")
+        nsplit_tot += nsplit
+        hits_tot += hits
+
+print(f"\nTOTAL {hits_tot:5} / {nsplit_tot:5} = {hits_tot / nsplit_tot * 100:6.3f}% complete")
+nsplit_tot += nsplit
+hits_tot += hits
