a739273000be05a53aa08a5673eca775f42b9f07
Reijo Keskitalo <reijo.keskitalo@gmail.com>
2022-11-21 16:30:31 -0800

    Support multiple sky models

--- dc1/foreground_sim/remove_timeouts.sh
+++ dc1/foreground_sim/remove_timeouts.sh
@@ -1,14 +1,34 @@
 #!/bin/bash
 
-fnames=(`grep -lR --include '*.log' -e '^TOAST INFO: Workflow completed in' logs/LAT0_CHLAT`)
-echo "Moving ${#fnames[@]} successful logs"
-for fname in ${fnames[*]}; do
-    echo "Moving $fname"
-    indir=`dirname $fname`
-    outdir=${indir/logs/cleared_logs}
-    echo "Moving $fname to $outdir" 
-    mkdir -p $outdir
-    mv $fname $outdir
+for suffix in "" "_lowcomplexity" "_highcomplexity"; do
+    echo "suffix = ${suffix}"
+
+    fnames=(`grep -lR --include '*.log' -e '^TOAST INFO: Workflow completed in' logs${suffix}/LAT0_CHLAT`)
+    echo "Moving ${#fnames[@]} successful logs"
+    for fname in ${fnames[*]}; do
+        echo "Moving $fname"
+        indir=`dirname $fname`
+        infile=`basename $fname`
+        outdir=${indir/logs/cleared_logs}
+        echo "Moving $fname to $outdir/$infile" 
+        mkdir -p $outdir
+        mv $fname $outdir
+    done
+    echo "Moved ${#fnames[@]} successful logs"
+
+    #fnames=(`grep -lR --include '*.log' -e 'DUE TO TIME LIMIT' logs/LAT0_CHLAT`)
+    fnames=(`find logs${suffix} -mmin +60 -name '*.log'`)
+    echo "Removing ${#fnames[@]} stalled logs"
+    for fname in ${fnames[*]}; do
+        echo "Removing $fname"
+        indir=`dirname $fname`
+        infile=`basename $fname`
+        outdir=${indir/logs/failed_logs}
+        echo "Moving $fname to $outdir/$infile" 
+        mkdir -p $outdir
+        mv $fname $outdir
+    done
+    echo "Removed ${#fnames[@]} stalled logs"
 done
 
 exit
@@ -18,8 +38,10 @@ echo "Removing ${#fnames[@]} failed logs"
 for fname in ${fnames[*]}; do
     echo "Removing $fname"
     indir=`dirname $fname`
+    infile=`basename $fname`
     outdir=${indir/logs/failed_logs}
-    echo "Moving $fname to $outdir" 
+    echo "Moving $fname to $outdir/$infile" 
     mkdir -p $outdir
     mv $fname $outdir
 done
+echo "Removed ${#fnames[@]} failed logs"
--- dc1/foreground_sim/setup_sim.py
+++ dc1/foreground_sim/setup_sim.py
@@ -11,36 +11,36 @@ os.makedirs(outdir, exist_ok=True)
 
 # CHLAT
 
-for band, freq in [
-        ("LFL1", 30),
-        ("LFL2", 40),
-        ("MFL1", 90),
-        ("MFL2", 150),
-        ("HFL1", 220),
-        ("HFL2", 280),
-]:
-    fname_out = os.path.join(outdir, f"foreground.chlat.f{freq:03}.h5")
-    if os.path.isfile(fname_out):
-        print(f"Output file exists: {fname_out}")
-        continue
-    print("WARNING: You MUST change the path to the correct foreground maps", flush=True)
-    fname_in = (
-        f"/global/cfs/cdirs/cmbs4/dm/mbs/"
-        f"202211_LAT_fg_cmb/4096/combined_foregrounds_cmb_no_radio_fixdip/0000/"
-        f"cmbs4_combined_foregrounds_cmb_no_radio_fixdip_uKCMB_LAT-{band}_nside4096_0000.fits"
-    )
-    if not os.path.isfile(fname_in):
-        raise RuntimeError(f"Input file does not exist: {fname_in}")
-    print(f"Reading {fname_in}")
-    m = read_map(
-        fname_in,
-        None,
-        nest=True,
-        verbose=False,
-        dtype=np.float32,
-    )
-    write_healpix(fname_out, m * 1e-6, coord="C", nest=True)
-    print(f"Wrote {fname_out}")
+for suffix in "", "_lowcomplexity", "_highcomplexity":
+    for band, freq in [
+            ("LFL1", 30),
+            ("LFL2", 40),
+            ("MFL1", 90),
+            ("MFL2", 150),
+            ("HFL1", 220),
+            ("HFL2", 280),
+    ]:
+        fname_out = os.path.join(outdir, f"foreground{suffix}.chlat.f{freq:03}.h5")
+        if os.path.isfile(fname_out):
+            print(f"Output file exists: {fname_out}")
+            continue
+        fname_in = (
+            f"/global/cfs/cdirs/cmbs4/dm/mbs/"
+            f"202211_LAT_fg_cmb/4096/combined_foregrounds_cmb_no_radio_fixdip{suffix}/0000/"
+            f"cmbs4_combined_foregrounds_cmb_no_radio_fixdip{suffix}_uKCMB_LAT-{band}_nside4096_0000.fits"
+        )
+        if not os.path.isfile(fname_in):
+            raise RuntimeError(f"Input file does not exist: {fname_in}")
+        print(f"Reading {fname_in}")
+        m = read_map(
+            fname_in,
+            None,
+            nest=True,
+            verbose=False,
+            dtype=np.float32,
+        )
+        write_healpix(fname_out, m * 1e-6, coord="C", nest=True)
+        print(f"Wrote {fname_out}")
 
 sys.exit()
 
