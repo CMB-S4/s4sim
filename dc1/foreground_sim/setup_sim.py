bc78b7f46fdf111acbcb6471069b749ca9c258c6
Reijo Keskitalo <reijo.keskitalo@gmail.com>
2022-12-08 16:08:57 -0800

    Preparing for more runs

--- dc1/foreground_sim/setup_sim.py
+++ dc1/foreground_sim/setup_sim.py
@@ -2,14 +2,24 @@ import os
 import sys
 
 import numpy as np
-from healpy import read_map
+from healpy import read_map, smoothing
 from toast.pixels_io_healpix import write_healpix
 
 
 outdir = "input_maps"
 os.makedirs(outdir, exist_ok=True)
+mask_frac = 0.15
+mask_fwhm = np.radians(1)
+mask_lmax = 512
 
 # CHLAT
+
+fname_dipo = (
+    "/global/cfs/cdirs/cmbs4/dm/mbs/"
+    "202211_LAT_fg_cmb/4096/"
+    "cmbs4_cmb_solar_dipole_uKCMB_nside4096.fits"
+)
+dipo = read_map(fname_dipo, nest=True)
 
 for suffix in "", "_lowcomplexity", "_highcomplexity":
     for band, freq in [
@@ -21,9 +31,9 @@ for suffix in "", "_lowcomplexity", "_highcomplexity":
             ("HFL2", 280),
     ]:
         fname_out = os.path.join(outdir, f"foreground{suffix}.chlat.f{freq:03}.h5")
-        if os.path.isfile(fname_out):
-            print(f"Output file exists: {fname_out}")
-            continue
+        #if os.path.isfile(fname_out):
+        #    print(f"Output file exists: {fname_out}")
+        #    continue
         fname_in = (
             f"/global/cfs/cdirs/cmbs4/dm/mbs/"
             f"202211_LAT_fg_cmb/4096/combined_foregrounds_cmb_no_radio_fixdip{suffix}/0000/"
@@ -39,8 +49,25 @@ for suffix in "", "_lowcomplexity", "_highcomplexity":
             verbose=False,
             dtype=np.float32,
         )
-        write_healpix(fname_out, m * 1e-6, coord="C", nest=True)
-        print(f"Wrote {fname_out}")
+        if not os.path.isfile(fname_out):
+            write_healpix(fname_out, m * 1e-6, coord="C", nest=True)
+            print(f"Wrote {fname_out}")
+
+        # Processing mask
+        fname_cmb = (
+            f"/global/cfs/cdirs/cmbs4/dm/mbs/"
+            f"202211_LAT_fg_cmb/4096/cmb/0000/"
+            f"cmbs4_cmb_uKCMB_LAT-{band}_nside4096_0000.fits"
+        )
+        cmb = read_map(fname_cmb, nest=True)
+        fg = m[0] - dipo - cmb
+        fg_sorted = np.sort(fg)
+        npix = fg.size
+        lim = fg_sorted[int(npix * (1 - mask_frac))]
+        mask = fg > lim
+        mask = smoothing(mask, lmax=mask_lmax, fwhm=mask_fwhm, nest=True) > 0.25
+        fname_out = os.path.join(outdir, f"mask{int(mask_frac * 100):02}{suffix}.chlat.f{freq:03}.h5")
+        write_healpix(fname_out, mask, dtype=np.int16, coord="C", nest=True)
 
 sys.exit()
 
