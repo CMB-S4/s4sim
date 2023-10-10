f4fb12c55184384ba89736cdd4cd8038814b9cbe
keskitalo <reijo.keskitalo@gmail.com>
2023-10-10 08:54:44 -0700

    enable all telescopes

--- chlat_rotation/setup.sh
+++ chlat_rotation/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# s4_hardware_to_toast3.py --telescope LAT0
-#s4_hardware_to_toast3.py --telescope SAT1
+s4_hardware_to_toast3.py --telescope LAT0
+s4_hardware_to_toast3.py --telescope SAT1
 s4_hardware_to_toast3.py --telescope LAT2
