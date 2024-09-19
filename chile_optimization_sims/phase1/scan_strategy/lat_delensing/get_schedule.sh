0741059a71016bba14e37fa0f399ebb000e155f3
keskitalo <reijo.keskitalo@gmail.com>
2024-09-19 12:48:37 -0700

    Tentative schedules

--- chile_optimization_sims/phase1/scan_strategy/lat_delensing/get_schedule.sh
+++ chile_optimization_sims/phase1/scan_strategy/lat_delensing/get_schedule.sh
@@ -10,7 +10,7 @@ toast_ground_schedule \
 
 toast_ground_schedule \
     @schedule_lat_delensing.par \
-    ../sat/patches_sat.txt  \
+    @../sat/patches_sat.txt  \
     --patch South_Direction,SIDEREAL,10.0,150.00,210.00,67.00,30,60,15 \
     --patch North_Direction,SIDEREAL,100.0,330.00,30.00,67.00,130,160,15 \
     --out schedule_lat_delensing_core.txt \
