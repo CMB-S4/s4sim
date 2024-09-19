59756b3d61ea13ecb1ed2bd2f81a02ce46e2cbac
keskitalo <reijo.keskitalo@gmail.com>
2024-09-19 11:14:11 -0700

    fix typos

--- chile_optimization_sims/phase1/scan_strategy/lat_delensing/get_schedule.sh
+++ chile_optimization_sims/phase1/scan_strategy/lat_delensing/get_schedule.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 
 toast_ground_schedule \
-    @chile_schedule_lat_delensing.par \
+    @schedule_lat_delensing.par \
     @patches_lat.txt \
     --patch South_Direction,SIDEREAL,10.0,150.00,210.00,67.00,30,60,15 \
     --patch North_Direction,SIDEREAL,100.0,330.00,30.00,67.00,130,160,15 \
@@ -9,7 +9,7 @@ toast_ground_schedule \
     >& schedule_lat_delensing.log
 
 toast_ground_schedule \
-    @chile_schedule_lat_delensing.par \
+    @schedule_lat_delensing.par \
     ../sat/patches_sat.txt  \
     --patch South_Direction,SIDEREAL,10.0,150.00,210.00,67.00,30,60,15 \
     --patch North_Direction,SIDEREAL,100.0,330.00,30.00,67.00,130,160,15 \
--- chile_optimization_sims/phase1/scan_strategy/sat/get_schedule.sh
+++ chile_optimization_sims/phase1/scan_strategy/sat/get_schedule.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 
 toast_ground_schedule \
-    @chile_schedule_sat.par \
+    @schedule_sat.par \
     @patches_sat.txt \
     --patch South_Direction,SIDEREAL,10.0,150.00,210.00,67.00,30,60,15 \
     --patch North_Direction,SIDEREAL,100.0,330.00,30.00,67.00,130,160,15 \
