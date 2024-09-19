4608d4768be4527bfc99f009d43474936dc7d85a
keskitalo <reijo.keskitalo@gmail.com>
2024-09-19 09:58:57 -0700

    Add sidereal targets

--- chile_optimization_sims/phase1/scan_strategy/sat/get_schedule.sh
+++ chile_optimization_sims/phase1/scan_strategy/sat/get_schedule.sh
@@ -3,5 +3,7 @@
 toast_ground_schedule \
     @chile_schedule_sat.par \
     @patches_sat.txt \
+    --patch South_Direction,SIDEREAL,10.0,150.00,210.00,67.00,30,60,15 \
+    --patch North_Direction,SIDEREAL,100.0,330.00,30.00,67.00,130,160,15 \
     --out schedule_sat.txt \
     >& schedule_sat.log
