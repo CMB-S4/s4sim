8a5122709c7ae82ef1d2f2f18d562890ef0ad6c2
keskitalo <reijo.keskitalo@gmail.com>
2024-09-24 14:16:13 -0700

    Remove North_Direction from targeting

--- chile_optimization_sims/phase1/scan_strategy/lat_delensing/get_schedule.sh
+++ chile_optimization_sims/phase1/scan_strategy/lat_delensing/get_schedule.sh
@@ -4,7 +4,6 @@ toast_ground_schedule \
     @schedule_lat_delensing.par \
     @patches_lat.txt \
     --patch South_Direction,SIDEREAL,10.0,150.00,210.00,67.00,30,60,15 \
-    --patch North_Direction,SIDEREAL,100.0,330.00,30.00,67.00,130,160,15 \
     --out schedule_lat_delensing.txt \
     >& schedule_lat_delensing.log
 
@@ -12,6 +11,7 @@ toast_ground_schedule \
     @schedule_lat_delensing.par \
     @../sat/patches_sat.txt  \
     --patch South_Direction,SIDEREAL,10.0,150.00,210.00,67.00,30,60,15 \
-    --patch North_Direction,SIDEREAL,100.0,330.00,30.00,67.00,130,160,15 \
     --out schedule_lat_delensing_core.txt \
     >& schedule_lat_delensing_core.log
+
+#     --patch North_Direction,SIDEREAL,100.0,330.00,30.00,67.00,130,160,15
