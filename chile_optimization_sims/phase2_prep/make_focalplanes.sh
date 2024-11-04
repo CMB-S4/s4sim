d0188bdd617422b4e583f30534632dd472bee81c
Reijo Keskitalo <reijo.keskitalo@gmail.com>
2024-11-04 15:52:15 -0800

    Automate generating focaplanes for all splits

--- chile_optimization_sims/phase2_prep/make_focalplanes.sh
+++ chile_optimization_sims/phase2_prep/make_focalplanes.sh
@@ -4,47 +4,44 @@
 
 # split3 = split4 = split5
 
-cd ../.. && git checkout Phase2_concepts && pip install . --prefix=$PREFIX && cd chile_optimization_sims/phase2_prep
-mkdir -p focalplanes_split3
-cd focalplanes_split3
+cd ../.. && git checkout Phase2_concepts && pip install . --prefix=$PREFIX
+mkdir -p temp/focalplanes_split3
+cd temp/focalplanes_split3
 s4_hardware_to_toast3.py --telescope SAT1 --by-tube
 s4_hardware_to_toast3.py --telescope SAT3 --by-tube
-cd ..
 
 # split6
 
-cd ../.. && git checkout split6 && pip install . --prefix=$PREFIX && cd chile_optimization_sims/phase2_prep
-mkdir -p focalplanes_split6
-cd focalplanes_split6
+cd ../.. && git checkout split6 && pip install . --prefix=$PREFIX
+mkdir -p temp/focalplanes_split6
+cd temp/focalplanes_split6
 s4_hardware_to_toast3.py --telescope SAT1 --by-tube
 s4_hardware_to_toast3.py --telescope SAT3 --by-tube
-cd ..
 
 # split7
 
-cd ../.. && git checkout split7 && pip install . --prefix=$PREFIX && cd chile_optimization_sims/phase2_prep
-mkdir -p focalplanes_split7
+cd ../.. && git checkout split7 && pip install . --prefix=$PREFIX
+mkdir -p temp/focalplanes_split7
 cd focalplanes_split7
 s4_hardware_to_toast3.py --telescope SAT1 --by-tube
 s4_hardware_to_toast3.py --telescope SAT3 --by-tube
-cd ..
 
 # split8 = split9
 
-cd ../.. && git checkout split89 && pip install . --prefix=$PREFIX && cd chile_optimization_sims/phase2_prep
+cd ../.. && git checkout split89 && pip install . --prefix=$PREFIX
 mkdir -p focalplanes_split8
 cd focalplanes_split8
 s4_hardware_to_toast3.py --telescope SAT1 --by-tube
 s4_hardware_to_toast3.py --telescope SAT3 --by-tube
-cd ..
 
 # split0 = split1 = split2
 # Run this last so we end up back in the master branch
 
-cd ../.. && git checkout master && pip install . --prefix=$PREFIX && cd chile_optimization_sims/phase2_prep
+cd ../.. && git checkout master && pip install . --prefix=$PREFIX
 mkdir -p focalplanes_split0
 cd focalplanes_split0
 s4_hardware_to_toast3.py --telescope LAT0
 s4_hardware_to_toast3.py --telescope SAT1 --by-tube
 s4_hardware_to_toast3.py --telescope SAT3 --by-tube
-cd ..
+cd chile_optimization_sims/phase2_prep
+rsync -avrP ../../temp/focalplanes_split? .
