284ad0b35bb94169ba643f7c5a8cc0c618bc3bad
Reijo Keskitalo <reijo.keskitalo@gmail.com>
2024-11-04 15:54:50 -0800

    Add missing directories

--- chile_optimization_sims/phase2_prep/make_focalplanes.sh
+++ chile_optimization_sims/phase2_prep/make_focalplanes.sh
@@ -29,7 +29,7 @@ s4_hardware_to_toast3.py --telescope SAT3 --by-tube
 # split8 = split9
 
 cd ../.. && git checkout split89 && pip install . --prefix=$PREFIX
-mkdir -p focalplanes_split8
+mkdir -p temp/focalplanes_split8
 cd focalplanes_split8
 s4_hardware_to_toast3.py --telescope SAT1 --by-tube
 s4_hardware_to_toast3.py --telescope SAT3 --by-tube
@@ -38,7 +38,7 @@ s4_hardware_to_toast3.py --telescope SAT3 --by-tube
 # Run this last so we end up back in the master branch
 
 cd ../.. && git checkout master && pip install . --prefix=$PREFIX
-mkdir -p focalplanes_split0
+mkdir -p temp/focalplanes_split0
 cd focalplanes_split0
 s4_hardware_to_toast3.py --telescope LAT0
 s4_hardware_to_toast3.py --telescope SAT1 --by-tube
