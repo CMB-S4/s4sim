d30869dc649b4b443ab67ef80f3eaa940019add7
Reijo Keskitalo <reijo.keskitalo@gmail.com>
2024-11-06 12:58:20 -0800

    Update scaling scripts

--- chile_optimization_sims/phase2_prep/sims/scale_maps.lat.py
+++ chile_optimization_sims/phase2_prep/sims/scale_maps.lat.py
@@ -9,220 +9,216 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-# Total target efficiency factor
+for isplit in [1, 2]:
+    split = f"split{isplit}"
+    flavors = {
+        "split1" : ["lat_wide", "lat_delensing"],
+        "split2" : ["lat_delensing"],
+    }[split]
 
-f_total = {}
+    # Total target efficiency factor
 
-# fsky for measurement requirement
+    f_total = {}
 
-fskies = {}
+    # fsky for measurement requirement
 
-# Factors not included in f_total
+    fskies = {}
 
-yield_ = 0.8
-f_weight = 0.8
+    # Factors not included in f_total
 
-# Efficiency factors already applied in the simulation must
-# not be double-counted in f_total
+    yield_ = 0.8
+    f_weight = 1.0
 
-f_season = 0.75
-f_field = {}
+    # Efficiency factors already applied in the simulation must
+    # not be double-counted in f_total
 
-# Weather cut is approximately included in the sim but we want to remove
-# it and replace with the factor included in f_total
+    f_season = 0.75
+    f_field = {}
 
-f_weather_sim = {}
+    # Weather cut is approximately included in the sim but we want to remove
+    # it and replace with the factor included in f_total
 
-# LAT wide f_total from
-# https://docs.google.com/spreadsheets/d/116Xa1vHrIwO6xTLsZalXo-QK7aKRQJnTE5LhkSl9eig/edit?gid=226211982#gid=226211982
-# on 09/27/2024
+    f_weather_sim = {}
 
-f_total["lat_wide"] = {
-    "f020" : 0.29,
-    "f030" : 0.29,
-    "f040" : 0.29,
-    "f090" : 0.29,
-    "f150" : 0.29,
-    "f220" : 0.25,
-    "f280" : 0.18,
-}
-f_field["lat_wide"] = 0.908
-fskies["lat_wide"] = 0.6
+    # LAT wide f_total from
+    # https://docs.google.com/spreadsheets/d/116Xa1vHrIwO6xTLsZalXo-QK7aKRQJnTE5LhkSl9eig/edit?gid=226211982#gid=226211982
+    # on 09/27/2024
 
-# LAT delensing f_total from
-# https://docs.google.com/spreadsheets/d/116Xa1vHrIwO6xTLsZalXo-QK7aKRQJnTE5LhkSl9eig/edit?gid=226211982#gid=226211982
-# on 09/27/2024
+    f_total["lat_wide"] = {
+        "f020" : 0.29,
+        "f030" : 0.29,
+        "f040" : 0.29,
+        "f090" : 0.29,
+        "f150" : 0.29,
+        "f220" : 0.25,
+        "f280" : 0.18,
+    }
+    f_field["lat_wide"] = 0.908
+    fskies["lat_wide"] = 0.6
 
-f_total["lat_delensing"] = {
-    "f020" : 0.30,
-    "f030" : 0.30,
-    "f040" : 0.30,
-    "f090" : 0.30,
-    "f150" : 0.30,
-    "f220" : 0.26,
-    "f280" : 0.18,
-}
-f_field["lat_delensing"] = 0.972
-fskies["lat_delensing"] = 0.03
+    # LAT delensing f_total from
+    # https://docs.google.com/spreadsheets/d/116Xa1vHrIwO6xTLsZalXo-QK7aKRQJnTE5LhkSl9eig/edit?gid=226211982#gid=226211982
+    # on 09/27/2024
 
-# LAT delensing_core is a variant of LAT delensing
+    f_total["lat_delensing"] = {
+        "f020" : 0.30,
+        "f030" : 0.30,
+        "f040" : 0.30,
+        "f090" : 0.30,
+        "f150" : 0.30,
+        "f220" : 0.26,
+        "f280" : 0.18,
+    }
+    f_field["lat_delensing"] = 0.972
+    fskies["lat_delensing"] = 0.03
 
-f_total["lat_delensing_core"] = {
-    "f020" : 0.29,
-    "f030" : 0.29,
-    "f040" : 0.29,
-    "f090" : 0.29,
-    "f150" : 0.29,
-    "f220" : 0.25,
-    "f280" : 0.18,
-}
-f_field["lat_delensing_core"] = 0.971
-fskies["lat_delensing_core"] = 0.03
+    # f_weather from the simulation logs on 11/06/2024
 
-# f_weather from the simulation logs on 09/27/2024
+    cut_3mm = 1 - 1218 / 6629
+    cut_2mm = 1 - 2067 / 6629
+    f_weather_sim["lat_wide"] = {
+        "f020" : cut_3mm,
+        "f030" : cut_3mm,
+        "f040" : cut_3mm,
+        "f090" : cut_3mm,
+        "f150" : cut_3mm,
+        "f220" : cut_2mm,
+        "f280" : cut_2mm,
+    }
+    if split == "split1":
+        cut_3mm = 1 - 1535 / 8494
+        cut_2mm = 1 - 2640 / 8494
+        f_weather_sim["lat_delensing"] = {
+            "f020" : cut_3mm,
+            "f030" : cut_3mm,
+            "f040" : cut_3mm,
+            "f090" : cut_3mm,
+            "f150" : cut_3mm,
+            "f220" : cut_2mm,
+            "f280" : cut_2mm,
+        }
+    elif split == "split2":
+        cut_3mm = 1 - 1549 / 8601
+        cut_2mm = 1 - 2645 / 8601
+        f_weather_sim["lat_delensing"] = {
+            "f020" : cut_3mm,
+            "f030" : cut_3mm,
+            "f040" : cut_3mm,
+            "f090" : cut_3mm,
+            "f150" : cut_3mm,
+            "f220" : cut_2mm,
+            "f280" : cut_2mm,
+        }
+    else:
+        raise RuntimeError(f"Unknown split: {split}")
 
-cut_3mm = 1 - 1169 / 6255
-cut_2mm = 1 - 1980 / 6255
-f_weather_sim["lat_wide"] = {
-    "f020" : cut_3mm,
-    "f030" : cut_3mm,
-    "f040" : cut_3mm,
-    "f090" : cut_3mm,
-    "f150" : cut_3mm,
-    "f220" : cut_2mm,
-    "f280" : cut_2mm,
-}
-cut_3mm = 1 - 1514 / 8361
-cut_2mm = 1 - 2592 / 8361
-f_weather_sim["lat_delensing"] = {
-    "f020" : cut_3mm,
-    "f030" : cut_3mm,
-    "f040" : cut_3mm,
-    "f090" : cut_3mm,
-    "f150" : cut_3mm,
-    "f220" : cut_2mm,
-    "f280" : cut_2mm,
-}
-cut_3mm = 1 - 1543 / 8481
-cut_2mm = 1 - 2655 / 8481
-f_weather_sim["lat_delensing_core"] = {
-    "f020" : cut_3mm,
-    "f030" : cut_3mm,
-    "f040" : cut_3mm,
-    "f090" : cut_3mm,
-    "f150" : cut_3mm,
-    "f220" : cut_2mm,
-    "f280" : cut_2mm,
-}
+    # Focalplane decimation factors must be compensated for
 
-# Focalplane decimation factors must be compensated for
+    thinfp = {
+        "f020" : 1,
+        "f030" : 1,
+        "f040" : 1,
+        "f090" : 16,
+        "f150" : 16,
+        "f220" : 16,
+        "f280" : 16,
+    }
 
-thinfp = {
-    "f020" : 1,
-    "f030" : 1,
-    "f040" : 1,
-    "f090" : 16,
-    "f150" : 16,
-    "f220" : 16,
-    "f280" : 16,
-}
+    # Number of telescope years is a parameter
 
-# Number of telescope years is a parameter
-
-n_years = {
-    "lat_wide" : [14],
-    "lat_delensing" : [16, 26, 36],
-    "lat_delensing_core" : [16, 26, 36],
-}
+    n_years = {
+        "lat_wide" : [14],
+        "lat_delensing" : [16, 26, 36],
+    }
 
 
-# Loop over all covariance matrices
+    # Loop over all covariance matrices
 
-for flavor in "lat_wide", "lat_delensing", "lat_delensing_core":
-    for n_year in n_years[flavor]:
-        nrow, ncol = 2, 4
-        fig = plt.figure(figsize=[4 * ncol, 4 * nrow])
-        fig.suptitle(f"{flavor} {n_year} years")
-        iplot = 0
-        for band in f_total[flavor].keys():
-            fname_in = f"outputs/{flavor}/{band}/mapmaker_cov.fits"
-            print(f"Loading {fname_in}")
-            cov = hp.read_map(fname_in, None)
+    for flavor in flavors:
+        for n_year in n_years[flavor]:
+            nrow, ncol = 2, 4
+            fig = plt.figure(figsize=[4 * ncol, 4 * nrow])
+            fig.suptitle(f"{flavor} {split} {n_year} years")
+            iplot = 0
+            for band in f_total[flavor].keys():
+                fname_in = f"outputs/{flavor}/{split}/{band}/mapmaker_cov.fits"
+                print(f"Loading {fname_in}")
+                cov = hp.read_map(fname_in, None)
 
-            # Scale the white noise covariance
+                # Scale the white noise covariance
 
-            scale = 1.
-            # Compensate for focalplane decimation
-            scale /= thinfp[band]
-            # Account for full mission length
-            scale /= n_year
-            # Yield and f_weight are not in f_total
-            scale /= yield_
-            scale /= f_weight
-            # Eliminate factors from simulation (included in f_total)
-            scale *= f_weather_sim[flavor][band]
-            scale *= f_season
-            scale *= f_field[flavor]
-            # Now apply f_total
-            scale /= f_total[flavor][band]
-            # Scale
-            cov *= scale
+                scale = 1.
+                # Compensate for focalplane decimation
+                scale /= thinfp[band]
+                # Account for full mission length
+                scale /= n_year
+                # Yield and f_weight are not in f_total
+                scale /= yield_
+                scale /= f_weight
+                # Eliminate factors from simulation (included in f_total)
+                scale *= f_weather_sim[flavor][band]
+                scale *= f_season
+                scale *= f_field[flavor]
+                # Now apply f_total
+                scale /= f_total[flavor][band]
+                # Scale
+                cov *= scale
 
-            # Save the scaled covariance
+                # Save the scaled covariance
 
-            outdir = f"scaled_outputs"
-            os.makedirs(outdir, exist_ok=True)
-            fname_out = f"{outdir}/{flavor}_{band}_{n_year}years_cov.fits"
-            print(f"Writing {fname_out}")
-            hp.write_map(fname_out, cov, dtype=np.float32, coord="C", overwrite=True)
+                outdir = f"scaled_outputs/lat/{split}"
+                os.makedirs(outdir, exist_ok=True)
+                fname_out = f"{outdir}/{flavor}_{band}_{n_year}years_cov.fits"
+                print(f"Writing {fname_out}")
+                hp.write_map(fname_out, cov, dtype=np.float32, coord="C", overwrite=True)
 
-            # Derive depth from the covariance and save
+                # Derive depth from the covariance and save
 
-            nside = hp.get_nside(cov)
-            pix_area = hp.nside2pixarea(nside, degrees=True) * 3600  # arcmin^2
-            depth_I = np.sqrt(cov[0] * pix_area) * 1e6  # uK.arcmin
-            depth_Q = np.sqrt(cov[3] * pix_area) * 1e6  # uK.arcmin
-            depth_U = np.sqrt(cov[5] * pix_area) * 1e6  # uK.arcmin
-            fname_out = f"{outdir}/{flavor}_{band}_{n_year}years_depth.fits"
-            print(f"Writing {fname_out}")
-            hp.write_map(
-                fname_out,
-                [depth_I, depth_Q, depth_U],
-                dtype=np.float32,
-                coord="C",
-                overwrite=True,
-            )
+                nside = hp.get_nside(cov)
+                pix_area = hp.nside2pixarea(nside, degrees=True) * 3600  # arcmin^2
+                depth_I = np.sqrt(cov[0] * pix_area) * 1e6  # uK.arcmin
+                depth_Q = np.sqrt(cov[3] * pix_area) * 1e6  # uK.arcmin
+                depth_U = np.sqrt(cov[5] * pix_area) * 1e6  # uK.arcmin
+                fname_out = f"{outdir}/{flavor}_{band}_{n_year}years_depth.fits"
+                print(f"Writing {fname_out}")
+                hp.write_map(
+                    fname_out,
+                    [depth_I, depth_Q, depth_U],
+                    dtype=np.float32,
+                    coord="C",
+                    overwrite=True,
+                )
 
-            # Plot depth
+                # Plot depth
 
-            iplot += 1
-            depth = depth_I
-            vmin = np.amin(depth[depth != 0])
-            vmax = 2 * vmin
-            #
-            sorted_depth = depth.copy()
-            sorted_depth[sorted_depth == 0] = 1e10
-            sorted_depth = np.sort(sorted_depth)
-            fsky = fskies[flavor]
-            lim = int(depth.size * fsky)
-            mean_depth = np.mean(sorted_depth[:lim])
-            #
-            depth[depth == 0] = hp.UNSEEN
-            hp.mollview(
-                depth,
-                min=vmin,
-                max=vmax,
-                title=f"{band}, mean = {mean_depth:.2f} (fsky={fsky})",
-                sub=[nrow, ncol, iplot],
-                cmap="inferno",
-                unit="$\mu$K.arcmin",
-                xsize=1600,
-                format="%.3f"
-            )
+                iplot += 1
+                depth = depth_I
+                vmin = np.amin(depth[depth != 0])
+                vmax = 2 * vmin
+                #
+                sorted_depth = depth.copy()
+                sorted_depth[sorted_depth == 0] = 1e10
+                sorted_depth = np.sort(sorted_depth)
+                fsky = fskies[flavor]
+                lim = int(depth.size * fsky)
+                mean_depth = np.mean(sorted_depth[:lim])
+                #
+                depth[depth == 0] = hp.UNSEEN
+                hp.mollview(
+                    depth,
+                    min=vmin,
+                    max=vmax,
+                    title=f"{band}, mean = {mean_depth:.2f} (fsky={fsky})",
+                    sub=[nrow, ncol, iplot],
+                    cmap="inferno",
+                    unit="$\mu$K.arcmin",
+                    xsize=1600,
+                    format="%.3f"
+                )
 
-        # Save plot
+            # Save plot
 
-        os.makedirs("plots", exist_ok=True)
-        fname_plot = f"plots/{flavor}_{n_year}years.png"
-        fig.savefig(fname_plot)
-        print(f"Plot saved in {fname_plot}")
+            os.makedirs("plots", exist_ok=True)
+            fname_plot = f"plots/{flavor}_{split}_{n_year}years.png"
+            fig.savefig(fname_plot)
+            print(f"Plot saved in {fname_plot}")
--- chile_optimization_sims/phase2_prep/sims/scale_maps.sat.py
+++ chile_optimization_sims/phase2_prep/sims/scale_maps.sat.py
@@ -9,189 +9,216 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-# Total target efficiency factor
+for isplit in range(8, 10):
+    split = f"split{isplit}"
 
-f_total = {}
+    # Total target efficiency factor
 
-# fsky for measurement requirement
+    f_total = {}
 
-fsky = 0.03
+    # fsky for measurement requirement
 
-# Factors not included in f_total
+    fsky = 0.03
 
-yield_ = 0.8
-f_weight = {
-    "f030" : 0.8,
-    "f040" : 0.8,
-    "f085" : 0.8,
-    "f095" : 0.8,
-    "f145" : 0.65,
-    "f155" : 0.65,
-    "f220" : 0.65,
-    "f280" : 0.65,
-}
+    # Factors not included in f_total
 
-# Efficiency factors already applied in the simulation must
-# not be double-counted in f_total
+    yield_ = 0.8
+    f_weight = 1.0
 
-f_season = 0.75
-f_field = {}
+    # Efficiency factors already applied in the simulation must
+    # not be double-counted in f_total
 
-# Weather cut is approximately included in the sim but we want to remove
-# it and replace with the factor included in f_total
+    f_season = 0.75
+    f_field = {}
 
-f_weather_sim = {}
+    # Weather cut is approximately included in the sim but we want to remove
+    # it and replace with the factor included in f_total
 
-# CHSAT f_total from
-# https://docs.google.com/spreadsheets/d/116Xa1vHrIwO6xTLsZalXo-QK7aKRQJnTE5LhkSl9eig/edit?gid=950965928#gid=950965928
-# on 10/07/2024
+    f_weather_sim = {}
 
-f_total["sat"] = {
-    "f030" : 0.17,
-    "f040" : 0.17,
-    "f085" : 0.17,
-    "f095" : 0.17,
-    "f145" : 0.16,
-    "f155" : 0.16,
-    "f220" : 0.10,
-    "f280" : 0.07,
-}
-f_field["sat"] = 0.54
+    # CHSAT f_total from
+    # https://docs.google.com/spreadsheets/d/116Xa1vHrIwO6xTLsZalXo-QK7aKRQJnTE5LhkSl9eig/edit?gid=950965928#gid=950965928
+    # on 10/07/2024
 
-# f_weather from the simulation logs on 10/07/2024
+    f_total["sat"] = {
+        "f030" : 0.17,
+        "f040" : 0.17,
+        "f085" : 0.17,
+        "f090" : 0.17,
+        "f095" : 0.17,
+        "f145" : 0.16,
+        "f150" : 0.16,
+        "f155" : 0.16,
+        "f220" : 0.10,
+        "f280" : 0.07,
+    }
+    f_field["sat"] = 0.54
 
-cut_3mm = 1 - 903 / 5157
-cut_2mm = 1 - 1525 / 5157
-f_weather_sim["sat"] = {
-    "f030" : cut_3mm,
-    "f040" : cut_3mm,
-    "f085" : cut_3mm,
-    "f095" : cut_3mm,
-    "f145" : cut_3mm,
-    "f155" : cut_3mm,
-    "f220" : cut_2mm,
-    "f280" : cut_2mm,
-}
+    # f_weather from the simulation logs on 11/06/2024
 
-# Focalplane decimation factors must be compensated for
+    if isplit in [0, 3, 6, 7, 8]:
+        cut_3mm = 1 - 955 / 5452
+        cut_2mm = 1 - 1636 / 5452
+        f_weather_sim["sat"] = {
+            "f030" : cut_3mm,
+            "f040" : cut_3mm,
+            "f085" : cut_3mm,
+            "f090" : cut_3mm,
+            "f095" : cut_3mm,
+            "f145" : cut_3mm,
+            "f150" : cut_3mm,
+            "f155" : cut_3mm,
+            "f220" : cut_2mm,
+            "f280" : cut_2mm,
+        }
+    elif isplit in [1, 4]:
+        cut_3mm = 1 - 1234 / 7078
+        cut_2mm = 1 - 2100 / 7078
+        f_weather_sim["sat"] = {
+            "f030" : cut_3mm,
+            "f040" : cut_3mm,
+            "f085" : cut_3mm,
+            "f090" : cut_3mm,
+            "f095" : cut_3mm,
+            "f145" : cut_3mm,
+            "f150" : cut_3mm,
+            "f155" : cut_3mm,
+            "f220" : cut_2mm,
+            "f280" : cut_2mm,
+        }
+    elif isplit in [2, 5, 9]:
+        cut_3mm = 1 - 1452 / 8087
+        cut_2mm = 1 - 2473 / 8087
+        f_weather_sim["sat"] = {
+            "f030" : cut_3mm,
+            "f040" : cut_3mm,
+            "f085" : cut_3mm,
+            "f090" : cut_3mm,
+            "f095" : cut_3mm,
+            "f145" : cut_3mm,
+            "f150" : cut_3mm,
+            "f155" : cut_3mm,
+            "f220" : cut_2mm,
+            "f280" : cut_2mm,
+        }
+    else:
+        raise RuntimeError(f"Unknown split: {isplit}")
 
-thinfp = {
-    "f030" : 1,
-    "f040" : 1,
-    "f085" : 1,
-    "f095" : 1,
-    "f145" : 1,
-    "f155" : 1,
-    "f220" : 1,
-    "f280" : 1,
-}
 
-# Number of telescope years is a parameter
+    # Focalplane decimation factors must be compensated for
 
-nsat = 9
-ntube_lf = nsat // 3
-ntube_mf = nsat
-ntube_hf = nsat // 3 * 2
-nyear = 10
-n_years = {
-    "f030" : nyear * ntube_lf,
-    "f040" : nyear * ntube_lf,
-    "f085" : nyear * ntube_mf,
-    "f095" : nyear * ntube_mf,
-    "f145" : nyear * ntube_mf,
-    "f155" : nyear * ntube_mf,
-    "f220" : nyear * ntube_hf,
-    "f280" : nyear * ntube_hf,
-}
+    thinfp = 1
+
+    # Number of telescope years is a parameter
+
+    nsat = 9
+    ntube_lf = nsat // 3
+    ntube_mf = nsat
+    ntube_hf = nsat // 3 * 2
+    nyear = 10
+    n_years = {
+        "f030" : nyear * ntube_lf,
+        "f040" : nyear * ntube_lf,
+        "f085" : nyear * ntube_mf,
+        "f090" : nyear * ntube_mf * 2,
+        "f095" : nyear * ntube_mf,
+        "f145" : nyear * ntube_mf,
+        "f150" : nyear * ntube_mf * 2,
+        "f155" : nyear * ntube_mf,
+        "f220" : nyear * ntube_hf,
+        "f280" : nyear * ntube_hf,
+    }
 
 
-# Loop over all covariance matrices
+    # Loop over all covariance matrices
 
-for flavor in "sat",:
-    nrow, ncol = 2, 4
-    fig = plt.figure(figsize=[4 * ncol, 4 * nrow])
-    fig.suptitle(f"{flavor}, {nsat * 3} tubes, fsky = {fsky}")
-    iplot = 0
-    for band in f_total[flavor].keys():
-        fname_in = f"outputs/{flavor}/{band}/mapmaker_cov.fits"
-        print(f"Loading {fname_in}")
-        cov = hp.read_map(fname_in, None)
+    for flavor in "sat",:
+        nrow, ncol = 2, 4
+        fig = plt.figure(figsize=[4 * ncol, 4 * nrow])
+        fig.suptitle(f"{flavor} {split}, {nsat * 3} tubes, fsky = {fsky}")
+        iplot = 0
+        for band in f_total[flavor].keys():
+            fname_in = f"outputs/{flavor}/{split}/{band}/mapmaker_cov.fits"
+            if not os.path.isfile(fname_in):
+                continue
+            print(f"Loading {fname_in}")
+            cov = hp.read_map(fname_in, None)
 
-        # Scale the white noise covariance
+            # Scale the white noise covariance
 
-        n_year = n_years[band]
+            n_year = n_years[band]
 
-        scale = 1.
-        # Compensate for focalplane decimation
-        scale /= thinfp[band]
-        # Account for full mission length
-        scale /= n_year
-        # Yield and f_weight are not in f_total
-        scale /= yield_
-        scale /= f_weight[band]
-        # Eliminate factors from simulation (included in f_total)
-        scale *= f_weather_sim[flavor][band]
-        scale *= f_season
-        scale *= f_field[flavor]
-        # Now apply f_total
-        scale /= f_total[flavor][band]
-        # Scale
-        cov *= scale
+            scale = 1.
+            # Compensate for focalplane decimation
+            scale /= thinfp
+            # Account for full mission length
+            scale /= n_year
+            # Yield and f_weight are not in f_total
+            scale /= yield_
+            scale /= f_weight
+            # Eliminate factors from simulation (included in f_total)
+            scale *= f_weather_sim[flavor][band]
+            scale *= f_season
+            scale *= f_field[flavor]
+            # Now apply f_total
+            scale /= f_total[flavor][band]
+            # Scale
+            cov *= scale
 
-        # Save the scaled covariance
+            # Save the scaled covariance
 
-        outdir = f"scaled_outputs"
-        os.makedirs(outdir, exist_ok=True)
-        fname_out = f"{outdir}/{flavor}_{band}_{n_year}years_cov.fits"
-        print(f"Writing {fname_out}")
-        hp.write_map(fname_out, cov, dtype=np.float32, coord="C", overwrite=True)
+            outdir = f"scaled_outputs/sat/{split}"
+            os.makedirs(outdir, exist_ok=True)
+            fname_out = f"{outdir}/{flavor}_{band}_{n_year}years_cov.fits"
+            print(f"Writing {fname_out}")
+            hp.write_map(fname_out, cov, dtype=np.float32, coord="C", overwrite=True)
 
-        # Derive depth from the covariance and save
+            # Derive depth from the covariance and save
 
-        nside = hp.get_nside(cov)
-        pix_area = hp.nside2pixarea(nside, degrees=True) * 3600  # arcmin^2
-        depth_I = np.sqrt(cov[0] * pix_area) * 1e6  # uK.arcmin
-        depth_Q = np.sqrt(cov[3] * pix_area) * 1e6  # uK.arcmin
-        depth_U = np.sqrt(cov[5] * pix_area) * 1e6  # uK.arcmin
-        fname_out = f"{outdir}/{flavor}_{band}_{n_year}years_depth.fits"
-        print(f"Writing {fname_out}")
-        hp.write_map(
-            fname_out,
-            [depth_I, depth_Q, depth_U],
-            dtype=np.float32,
-            coord="C",
-            overwrite=True,
-        )
+            nside = hp.get_nside(cov)
+            pix_area = hp.nside2pixarea(nside, degrees=True) * 3600  # arcmin^2
+            depth_I = np.sqrt(cov[0] * pix_area) * 1e6  # uK.arcmin
+            depth_Q = np.sqrt(cov[3] * pix_area) * 1e6  # uK.arcmin
+            depth_U = np.sqrt(cov[5] * pix_area) * 1e6  # uK.arcmin
+            fname_out = f"{outdir}/{flavor}_{band}_{n_year}years_depth.fits"
+            print(f"Writing {fname_out}")
+            hp.write_map(
+                fname_out,
+                [depth_I, depth_Q, depth_U],
+                dtype=np.float32,
+                coord="C",
+                overwrite=True,
+            )
 
-        # Plot depth
+            # Plot depth
 
-        iplot += 1
-        depth = depth_I * np.sqrt(2)  # depth_P
-        vmin = np.amin(depth[depth != 0])
-        vmax = 2 * vmin
-        #
-        sorted_depth = depth.copy()
-        sorted_depth[sorted_depth == 0] = 1e10
-        sorted_depth = np.sort(sorted_depth)
-        lim = int(depth.size * fsky)
-        mean_depth = np.mean(sorted_depth[:lim])
-        #
-        depth[depth == 0] = hp.UNSEEN
-        hp.mollview(
-            depth,
-            min=vmin,
-            max=vmax,
-            title=f"{band}, {n_year} tube years, mean = {mean_depth:.2f}",
-            sub=[nrow, ncol, iplot],
-            cmap="inferno",
-            unit="$\mu$K.arcmin",
-            xsize=1600,
-            format="%.3f"
-        )
+            iplot += 1
+            depth = depth_I * np.sqrt(2)  # depth_P
+            vmin = np.amin(depth[depth != 0])
+            vmax = 2 * vmin
+            #
+            sorted_depth = depth.copy()
+            sorted_depth[sorted_depth == 0] = 1e10
+            sorted_depth = np.sort(sorted_depth)
+            lim = int(depth.size * fsky)
+            mean_depth = np.mean(sorted_depth[:lim])
+            #
+            depth[depth == 0] = hp.UNSEEN
+            hp.mollview(
+                depth,
+                min=vmin,
+                max=vmax,
+                title=f"{band}, {n_year} tube years, mean = {mean_depth:.2f}",
+                sub=[nrow, ncol, iplot],
+                cmap="inferno",
+                unit="$\mu$K.arcmin",
+                xsize=1600,
+                format="%.3f"
+            )
 
-    # Save plot
+        # Save plot
 
-    os.makedirs("plots", exist_ok=True)
-    fname_plot = f"plots/{flavor}.png"
-    fig.savefig(fname_plot)
-    print(f"Plot saved in {fname_plot}")
+        os.makedirs("plots", exist_ok=True)
+        fname_plot = f"plots/{flavor}_{split}.png"
+        fig.savefig(fname_plot)
+        print(f"Plot saved in {fname_plot}")
