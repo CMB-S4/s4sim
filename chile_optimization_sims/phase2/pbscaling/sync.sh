8767b105fba42ccc6142e1a0f65135f8e00f4c9b
Reijo Keskitalo <reijo.keskitalo@gmail.com>
2025-02-14 13:18:05 -0800

    Add plotting script

--- chile_optimization_sims/phase2/pbscaling/sync.sh
+++ chile_optimization_sims/phase2/pbscaling/sync.sh
@@ -2,7 +2,7 @@
 
 
 #for flavor in cmb with_pbscaling no_pbscaling; do
-for flavor in cmb; do
+for flavor in no_pbscaling_no_artifact; do
     rsync -avrP $flavor /global/cfs/cdirs/cmbs4/chile_optimization/simulations/phase2/
     chmod g+rX-w -R /global/cfs/cdirs/cmbs4/chile_optimization/simulations/phase2/$flavor
     chgrp cmbs4 -R /global/cfs/cdirs/cmbs4/chile_optimization/simulations/phase2/$flavor
