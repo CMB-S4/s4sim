35848e3a0110dc7e3919e23d083d79511cd441cd
keskitalo <reijo.keskitalo@gmail.com>
2025-04-28 17:30:16 -0700

    Add schedule targeting the Roman field

---
+++ chile_optimization_sims/phase3/scan_strategy/lat_roman/get_schedule.sh
@@ -0,0 +1,22 @@
+#/bin/bash
+
+# This schedule targets the Northern Roman wide field
+# https://roman.gsfc.nasa.gov/science/High_Latitude_Wide_Area_Survey.html
+
+toast_ground_schedule \
+    @schedule_lat.par \
+    --patch roman_north_full,1,200.0,2.0,120.0,-20.000 \
+    --patch roman_north_000,0.001,200.0,2.0,190.0,-20.000 \
+    --patch roman_north_001,0.001,190.0,2.0,180.0,-20.000 \
+    --patch roman_north_002,0.001,180.0,2.0,170.0,-20.000 \
+    --patch roman_north_003,0.001,170.0,2.0,160.0,-20.000 \
+    --patch roman_north_004,0.001,160.0,2.0,150.0,-20.000 \
+    --patch roman_north_005,0.001,150.0,2.0,140.0,-20.000 \
+    --patch roman_north_006,0.001,140.0,2.0,130.0,-20.000 \
+    --patch roman_north_007,0.001,130.0,2.0,120.0,-20.000 \
+    --ra-period 5 --ra-amplitude 5 \
+    --out schedule_lat_roman.txt \
+    >& schedule_lat_roman.log &
+
+# --block-out 01/01-04/01
+# python gapfill_schedule.py schedule_lat_wide.txt ../lat_delensing_max/schedule_lat_delensing_max.txt schedule_lat_delensing_supplement.txt
